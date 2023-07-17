789ad420f3b9774e548d4e4c4d3b951f529dddcc
Melyne99 <melyne.marques1999@gmail.com>
2023-07-17 16:30:13 +0200

    rajout dossier albums, css lecteur + js

--- htdocs/home.php
+++ htdocs/home.php
@@ -1,2 +1,45 @@
 <?php
 include 'connexion.php';
+?>
+
+<!DOCTYPE html>
+<html>
+  <title>Home</title>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylecss/home.css">
+
+</head>
+<body>
+<section class="wrapper">
+
+<article class="music-container">
+
+    <div class="controls">
+        <input type="radio" name="controls" id="btn-play">
+        <label for="btn-play" class="lbl-btn-play"><span></span></label>
+
+        <input type="radio" name="controls" id="btn-pause">
+        <label for="btn-pause" class="lbl-btn-pause"><span></span></label>
+
+        <label class="lbl-btn-reset"><span></span></label>
+    </div>
+
+    <div class="cover">
+        <div class="static-card">
+            <img src="http://f1.bcbits.com/img/a1312167393_16.jpg" alt="">
+        </div>
+        <div class="flip-card">
+            <img src="http://f1.bcbits.com/img/a1312167393_16.jpg" alt="">
+        </div>
+    </div>
+</article>
+
+<p class="info">Hover/Click on album cover to show controls</p>
+
+</section>
+
+<script src="lecteur.js"></script>
+</body>
+</html>
--- htdocs/traitement.php
+++ htdocs/traitement.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 include 'connexion.php';
-if (isset($_POST['pseudo'])) {
 
+if (isset($_POST['pseudo'])) {
     $pseudo = $_POST['pseudo'];
+
     $query = $bdd->prepare("SELECT * FROM Users WHERE pseudo = :pseudo"); 
-    $query->execute([
-        ':pseudo' => $pseudo
-    ]);
+    $query->execute([':pseudo' => $pseudo]);
     $user = $query->fetch(PDO::FETCH_ASSOC);
 
     if (!$user) {
@@ -15,9 +14,9 @@ if (isset($_POST['pseudo'])) {
         $insertQuery->execute([':pseudo' => $pseudo]);
         $userid = $bdd->lastInsertId();
     } else {
-        $userid = $user['id'];
+        $userid = $user['user_id'];
     }
 
     $_SESSION['user_id'] = $userid;
-    header('Location: index.php');
 }
+header('location: home.php?userid='.$userid);
