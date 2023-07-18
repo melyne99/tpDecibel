27391d0bf9c22272b61ed39234e28a84a0a376ee
YvanNours~ <yvan.bardel@gmail.com>
2023-07-18 16:16:11 +0200

    modif navbar, supp dossier album,changement css

---
+++ htdocs/navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="stylecss/navbar.css">
+  <!-- Navbar -->
+<div class="navbar">
+  <a href="index.php">Decibel</a>
+  <div class="search-container">
+    <ul action="/search">
+      <input type="text" placeholder="Recherche..." name="search">
+      <button type="submit">Go</button>
+    </ul>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Menu</button>
+    <div class="dropdown-content">
+      <a href="#album">Album</a>
+      <a href="#profil">Profil</a>
+      <a href="#playlist">Playliste</a>
+    </div>
+  </div>
+</div>
+</head>
+<body>
+</body>
+</html>
