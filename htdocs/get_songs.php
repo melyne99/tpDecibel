8adbb9b6061ef7dfa4a6bd8cb11fb3486d3ca30f
Melyne99 <melyne.marques1999@gmail.com>
2023-07-19 14:09:26 +0200

    changement back php pour lecture fichier audio

--- htdocs/get_songs.php
+++ htdocs/get_songs.php
@@ -2,7 +2,7 @@
 include 'connexion.php';
 
 $query = $bdd->query("SELECT * FROM Songs");
-$morceaux = $query->fetchAll(PDO::FETCH_ASSOC);
+$songs = $query->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($morceaux);
+echo($songs[]['title'])
 ?>
