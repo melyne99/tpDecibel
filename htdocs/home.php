018ab659984b858e769ad4a04f938619f49d7b8e
Melyne99 <melyne.marques1999@gmail.com>
2023-07-20 09:56:21 +0200

    rajout boite de dialog

--- htdocs/home.php
+++ htdocs/home.php
@@ -15,7 +15,7 @@ if ($result->rowCount() > 0) {
     echo '<ul>';
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         $chemins_audio[] = $row['file_path'];
-        echo '<li><a href="#" onclick="playAudio(' . count($chemins_audio) . ')">' . $row['title'] . '</a></li>';
+        echo '<li><a href="#" onclick="playAudio(' . (count($chemins_audio) - 1) . ')">' . $row['title'] . '</a></li>';
     }
     echo '</ul>';
 } else {
@@ -27,7 +27,9 @@ if ($result->rowCount() > 0) {
 <html>
 <head>
     <title>Lecteur audio</title>
-
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylecss/dialog.css">
     <link rel="stylesheet" href="stylecss/home.css">
 </head>
 <body>
@@ -40,31 +42,39 @@ if ($result->rowCount() > 0) {
         Votre navigateur ne prend pas en charge l'élément audio.
     </audio>
 
+    <!-- Bouton pour ouvrir la boîte de dialogue -->
+    <button onclick="openCommentModal()">Commenter</button>
+
+    <!-- Boîte de dialogue modale -->
+    <div id="commentModal" class="modal">
+        <div class="modal-content">
+            <span class="close" onclick="closeCommentModal()">&times;</span>
+            <h2>Commenter la chanson</h2>
+            <textarea id="commentText" rows="4" cols="50"></textarea>
+            <button onclick="saveComment()">Enregistrer</button>
+        </div>
+    </div>
+
     <script>
-        // Tableau pour stocker les chemins des fichiers audio
-        let cheminsAudio = <?php echo json_encode($chemins_audio); ?>;
+        // ... Votre code JavaScript ici ...
 
-        // Index de la chanson actuellement en cours de lecture
-        let currentIndex = 0;
-
-        // Fonction pour jouer le fichier audio
-        function playAudio(index) {
-            let lecteurAudio = document.getElementById('lecteur-audio');
-            lecteurAudio.src = cheminsAudio[index];
-            lecteurAudio.play();
-            currentIndex = index;
+        // Fonction pour ouvrir la boîte de dialogue
+        function openCommentModal() {
+            var modal = document.getElementById('commentModal');
+            modal.style.display = 'block';
         }
 
-        // Fonction pour jouer la chanson suivante
-        function nextAudio() {
-            currentIndex = (currentIndex + 1) % cheminsAudio.length;
-            playAudio(currentIndex);
+        // Fonction pour fermer la boîte de dialogue
+        function closeCommentModal() {
+            var modal = document.getElementById('commentModal');
+            modal.style.display = 'none';
         }
 
-        // Fonction pour jouer la chanson précédente
-        function previousAudio() {
-            currentIndex = (currentIndex - 1 + cheminsAudio.length) % cheminsAudio.length;
-            playAudio(currentIndex);
+        // Fonction pour enregistrer le commentaire
+        function saveComment() {
+            var commentText = document.getElementById('commentText').value;
+            // Ici, vous pouvez enregistrer le commentaire dans votre base de données ou effectuer toute autre action souhaitée avec le commentaire
+            closeCommentModal(); // Ferme la boîte de dialogue après avoir enregistré le commentaire
         }
     </script>
 </body>
